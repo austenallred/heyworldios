25e9392f64f012e4127378f6523e488fc1bd7cde
Austen Allred <austen.allred@gmail.com>
2015-01-16 15:30:20 -0700

    Initial Commit

---
+++ HeyWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeyWorld
+//
+//  Created by Austen Allred on 1/16/15.
+//  Copyright (c) 2015 Austen Allred. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
